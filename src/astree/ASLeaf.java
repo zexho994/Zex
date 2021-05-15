7355592a4043506cf10dbc6295f6f86dbf21129b
zexho <zouzhihao994@gmail.com>
2021-05-15 11:29:01 +0800

    update: add ASLeaf class

---
+++ src/astree/ASLeaf.java
@@ -0,0 +1,43 @@
+package astree;
+
+import token.Token;
+
+import java.util.Collections;
+import java.util.Iterator;
+
+/**
+ * @author Zexho
+ * @date 2021/5/15 11:20 上午
+ */
+public class ASLeaf extends ASTree {
+
+    protected final Token token;
+
+    public ASLeaf(Token t) {
+        this.token = t;
+    }
+
+    @Override
+    public ASTree child(int i) {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public int numChildren() {
+        return 0;
+    }
+
+    @Override
+    public Iterator<ASTree> children() {
+        return Collections.emptyIterator();
+    }
+
+    @Override
+    public String location() {
+        return "at line " + token.getLineNumber();
+    }
+
+    public Token token() {
+        return token;
+    }
+}
