7bea8e0e611411a2ebcc1ce2bede865370d14717
zexho <zouzhihao994@gmail.com>
2021-05-15 11:28:50 +0800

    update: add ASTree class

---
+++ src/astree/ASTree.java
@@ -0,0 +1,33 @@
+package astree;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author Zexho
+ * @date 2021/5/15 11:12 上午
+ */
+public abstract class ASTree implements Iterable<ASTree> {
+    /**
+     * get the i-th child node
+     */
+    public abstract ASTree child(int i);
+
+    /**
+     * get the number of children
+     */
+    public abstract int numChildren();
+
+    /**
+     * return an iterator for traversing child nodes
+     */
+    public abstract Iterator<ASTree> children();
+
+    public abstract String location();
+
+    @Override
+    public Iterator<ASTree> iterator() {
+        return children();
+    }
+
+}
