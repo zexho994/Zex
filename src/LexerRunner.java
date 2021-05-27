210d8665682d8d392c5b1551223f6e7f6c4f5f36
zexho <zouzhihao994@gmail.com>
2021-05-27 20:02:42 +0800

    rename LexerRunner

--- src/LexerRunner.java
+++ src/LexerRunner.java
@@ -5,7 +5,7 @@ import token.Token;
  * @author Zexho
  * @date 2021/5/14 5:28 下午
  */
-public class LexerRuner {
+public class LexerRunner {
     public static void main(String[] args) throws ParseException {
         CodeDialog codeDialog = new CodeDialog();
         Lexer l = new Lexer(codeDialog);
