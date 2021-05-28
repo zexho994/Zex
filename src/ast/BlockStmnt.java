4065db9fbd9a2a6b1a0b40ae846e7c59a4f0fa61
zexho <zouzhihao994@gmail.com>
2021-05-28 08:42:02 +0800

    add BlockStmnt

---
+++ src/ast/BlockStmnt.java
@@ -0,0 +1,13 @@
+package ast;
+
+import java.util.List;
+
+/**
+ * @author Zexho
+ * @date 2021/5/28 8:32 上午
+ */
+public class BlockStmnt extends ASTList {
+    public BlockStmnt(List<ASTree> c) {
+        super(c);
+    }
+}
