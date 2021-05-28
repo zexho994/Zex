111e7229e3c04289df0682a61184ebfcd313adb4
zexho <zouzhihao994@gmail.com>
2021-05-28 08:41:36 +0800

    add NullStmnt

---
+++ src/ast/NullStmnt.java
@@ -0,0 +1,13 @@
+package ast;
+
+import java.util.List;
+
+/**
+ * @author Zexho
+ * @date 2021/5/28 8:34 上午
+ */
+public class NullStmnt extends ASTList {
+    public NullStmnt(List<ASTree> c) {
+        super(c);
+    }
+}
