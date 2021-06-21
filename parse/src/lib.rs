aeaf9b49eba21b03db8c05023a09c598b8b39615
zexho <zouzhihao994@gmail.com>
2021-06-21 09:14:29 +0800

    parse to ast

--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -20,9 +20,9 @@ mod tests {
 
     #[test]
     fn parse_tokens_to_ast() {
-        let mut tokens = token::new_tokens(String::from("int a = 1 +  2 * 3"));
-        assert_eq!(tokens.count(), 8);
-        parse::parse_tokens_to_ast(&mut tokens);
+        let tokens = token::new_tokens(String::from("int a = 1 * 3"));
+        let ast = parse::parse_tokens_to_ast(tokens);
+        println!("ast is -> {:?}", ast);
     }
 
     #[test]
@@ -33,23 +33,12 @@ mod tests {
 
     #[test]
     fn ast(){
-        let mut tokens = token::new_tokens(String::from("int a = 1 +  2 * 3"));
+        let mut tokens = token::new_tokens(String::from("2 * 3"));
         let mut root = parse::new_ast_node(tokens.read().unwrap());
         match root.get_child(0) {
             None => {}
             _ => {panic!("root get child should be None")}
         }
-        root.add_child(tokens.read().unwrap());
-        let c1 = root.get_child(0).unwrap();
-        c1.add_child(tokens.read().unwrap());
-        let c2 = c1.get_child(0).unwrap();
-        c2.add_child(tokens.read().unwrap());
-        match c2.get_child(0) {
-            Some(t) => {
-                 t.add_child(tokens.read().unwrap());
-                 println!("t is {:?}",t); 
-            }
-            _ => {panic!("ast failed")}
-        }
+        
     }
 }
--- parse/src/parse.rs
+++ parse/src/parse.rs
@@ -12,11 +12,6 @@ pub fn new_ast_node(root: Token) -> AstNode {
     AstNode {_token: root, _child: Vec::new()}
 }
 
-pub fn add_child(ast: &mut AstNode,t: Token) {
-    let t = new_ast_node(t);
-    ast._child.push(t);
-}
-
 impl AstNode {
     pub fn get_child(&mut self,i: usize) -> Option<&mut AstNode> {
         return if self._child.len() <= i {
@@ -26,15 +21,15 @@ impl AstNode {
         }
     }
 
-    pub fn add_child(&mut self,child: Token) {
-        self._child.push(new_ast_node(child));
+    pub fn add_child(&mut self,child: AstNode) {
+        self._child.push(child);
     }
 }
 
-pub fn parse_tokens_to_ast(tokens: &mut Tokens) {
+pub fn parse_tokens_to_ast(mut tokens: Tokens) -> AstNode{
     match tokens.peek().unwrap()._type {
         TokenType::Int => {
-            int_variable_declaration(tokens);
+            int_variable_declaration(&mut tokens)
         }
         _ => {
             panic!("parse_tokens_to_ast failed");
@@ -44,10 +39,12 @@ pub fn parse_tokens_to_ast(tokens: &mut Tokens) {
 
 /// ## 语法规则
 /// int {identifier} {eq} {expr}
-fn int_variable_declaration(tokens: &mut Tokens) {
+fn int_variable_declaration(tokens: &mut Tokens) -> AstNode {
     let int = tokens.read().unwrap();
     let id = tokens.read().unwrap();
     let eq = tokens.read().unwrap();
+    // match add
+    match_add_expr(tokens)
 }
 
 /// ## BNF
@@ -55,8 +52,8 @@ fn int_variable_declaration(tokens: &mut Tokens) {
 /// :   mul
 /// |   add Plus mul
 /// ;
-fn match_add_expr(tokens: &mut Tokens) {
-    match_mul_expr(tokens);
+fn match_add_expr(tokens: &mut Tokens) -> AstNode {
+    match_mul_expr(tokens)
 }
 
 /// ## BNF
@@ -64,19 +61,33 @@ fn match_add_expr(tokens: &mut Tokens) {
 /// :   int
 /// |   mul Star int
 /// ;
-pub fn match_mul_expr(tokens: &mut Tokens) {
+pub fn match_mul_expr(tokens: &mut Tokens) -> AstNode {
+    // 预读
     match tokens.peek() {
-        Option::Some(_token) => {
-            match _token._type {
+        Option::Some(t1) => {
+            // 当前token的类型
+            match t1._type {
                 TokenType::Number => {
-                    tokens.read();
+                    let left = new_ast_node(tokens.read().unwrap());
                     let next = tokens.peek();
-                    println!("next is {:?}", next)
+                    match next {
+                        Option::Some(t2) => {
+                            match t2._type {
+                                TokenType::Star => {
+                                    let mut r = new_ast_node(tokens.read().unwrap());
+                                    r.add_child(left);
+                                    r.add_child(match_mul_expr(tokens));
+                                    return r;
+                                }
+                                _ => {panic!("match_mul_expr failed")}
+                            }
+                        }
+                        Option::None =>{return left}
+                    }
                 }
                 _ => { panic!("match_mul_expr failed") }
             }
         }
-        Option::None => { return; }
         _ => { panic!("match_mul_expr failed") }
     }
 }
