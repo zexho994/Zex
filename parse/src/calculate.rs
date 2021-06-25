fa08c241704e6baafe786d26b9ad60ef3d009650
zexho <zouzhihao994@gmail.com>
2021-06-25 22:27:54 +0800

    拆分calculate_int_declare

--- parse/src/calculate.rs
+++ parse/src/calculate.rs
@@ -3,14 +3,15 @@ use ast_node::*;
 use ast_node_type::*;
 use std::collections::HashMap;
 
+/// 执行program，支持表达式计算
 pub fn calculate_prog(ast_root: &mut AstNode) -> Option<i32> {
 	let mut var_map: HashMap<String, i32> = HashMap::new();
-	let output: i32 = 0;
+	let mut output: i32 = 0;
 	for stmt in ast_root._child.iter_mut() {
 		// 根据不同的type进行不同的计算
 		match stmt._type {
 			AstNodeType::IntDeclaration => {
-				calculate_int_declare(stmt, &mut var_map);
+				output = calculate_int_declare(stmt, &mut var_map);
 			}
 			AstNodeType::AssignmentStmt => {}
 			AstNodeType::ExpressionStmt => {}
@@ -22,56 +23,40 @@ pub fn calculate_prog(ast_root: &mut AstNode) -> Option<i32> {
 
 /// 计算Ast的值
 fn calculate_int_declare(ast: &mut AstNode, var_map: &mut HashMap<String, i32>) -> i32 {
+	let id = ast._text.clone();
+	let old = *var_map.get(&id).or(Option::Some(&0)).unwrap();
+	let l = match ast.get_child(0) {
+		Some(node) => calculate_sum(node, var_map),
+		None => 0,
+	};
+	let r = match ast.get_child(1) {
+		Some(node) => calculate_sum(node, var_map),
+		None => 0,
+	};
+	var_map.insert(id, old + l + r);
+	old + l + r
+}
+
+fn calculate_sum(ast: &mut AstNode, var_map: &mut HashMap<String, i32>) -> i32 {
 	match ast._type {
-		AstNodeType::IntDeclaration => {
-			match var_map.contains_key(&ast._text) {
-				// 变量存在
-				true => {
-					let old = *var_map.get(&ast._text).unwrap();
-					let l = match ast.get_child(0) {
-						Some(node) => calculate_int_declare(node, var_map),
-						None => 0,
-					};
-					let r = match ast.get_child(1) {
-						Some(node) => calculate_int_declare(node, var_map),
-						None => 0,
-					};
-					var_map.insert(ast._text.as_str().to_string(), old + l + r);
-					old + l + r
-				}
-				// 变量不存在
-				false => {
-					let l = match ast.get_child(0) {
-						Some(node) => calculate_int_declare(node, var_map),
-						None => 0,
-					};
-					let r = match ast.get_child(1) {
-						Some(node) => calculate_int_declare(node, var_map),
-						None => 0,
-					};
-					var_map.insert(ast._text.as_str().to_string(), l + r);
-					l + r
-				}
-			}
-		}
 		AstNodeType::Additive => {
 			let l = match ast.get_child(0) {
-				Some(node) => calculate_int_declare(node, var_map),
+				Some(node) => calculate_sum(node, var_map),
 				None => 0,
 			};
 			let r = match ast.get_child(1) {
-				Some(node) => calculate_int_declare(node, var_map),
+				Some(node) => calculate_sum(node, var_map),
 				None => 0,
 			};
 			l + r
 		}
 		AstNodeType::Multiplicative => {
 			let l = match ast.get_child(0) {
-				Some(node) => calculate_int_declare(node, var_map),
-				None => 0,
+				Some(node) => calculate_sum(node, var_map),
+				None => 1,
 			};
 			let r = match ast.get_child(1) {
-				Some(node) => calculate_int_declare(node, var_map),
+				Some(node) => calculate_sum(node, var_map),
 				None => 1,
 			};
 			l * r
@@ -80,3 +65,10 @@ fn calculate_int_declare(ast: &mut AstNode, var_map: &mut HashMap<String, i32>) 
 		_ => panic!("calculate error, p is {:?}", ast),
 	}
 }
+
+///
+// fn calculate_assignment(ast: &mut AstNode, var_map: &mut HashMap<String, i32>) -> i32 {
+// 	let id = ast._text;
+// }
+
+fn calculate_additive(ast: &mut AstNode, var_map: &mut HashMap<String, i32>) {}
