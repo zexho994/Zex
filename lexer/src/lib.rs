b479144a76e235ac6016e87898ac2a9179d30df6
zexho <zouzhihao994@gmail.com>
2021-06-29 23:34:27 +0800

    [refactor]<lib> 更新测试用例

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -11,25 +11,15 @@ mod test {
     use super::*;
 
     #[test]
-    fn parse_test_1() {
-        let str1 = String::from("age >= 15 ;");
-        let tokens = lexing(str1);
-        assert_eq!(tokens.count(), 4)
-    }
-
-    #[test]
-    fn parse_test_5() {
-        let str1 = String::from("inte num > 01;");
-        let tokens = lexing(str1);
-        assert!(tokens.data.len() == 5);
-    }
-
-    #[test]
-    fn parse_test_7() {
-        let s = String::from("i int if num > 01;");
+    fn lexing_flow() {
+        let s = String::from("i int if num > 01; {}");
         let tokens = lexing(s.as_str().to_string());
         println!("tokens {:?}", tokens);
-        assert!(tokens.data.len() == 7);
+        assert!(tokens.data.len() == 9);
+        match tokens.get_child_idx(0).unwrap()._type {
+            TokenType::Identifier => {}
+            _ => panic!("parse s = {} failed", s),
+        }
         match tokens.get_child_idx(1).unwrap()._type {
             TokenType::Int => {}
             _ => panic!("parse s = {} failed", s),
@@ -38,75 +28,32 @@ mod test {
             TokenType::IF => {}
             _ => panic!("parse s = {} failed", s),
         }
-    }
-
-    #[test]
-    fn parse_test_12() {
-        let str1 = String::from("int num=1; ");
-        let tokens = lexing(str1);
-        println!("tokens is {:?}", tokens);
-        assert!(tokens.data.len() == 5);
-    }
-
-    #[test]
-    fn assignment() {
-        let str1 = String::from("=");
-        let tokens = lexing(str1);
-        assert!(tokens.data.len() == 1);
-        match tokens.peek() {
-            Some(t) => match t._type {
-                TokenType::Assignment => {
-                    println!("");
-                }
-                _ => panic!("token is not an Assignment"),
-            },
-            None => panic!("parse test fail"),
+        match tokens.get_child_idx(3).unwrap()._type {
+            TokenType::Identifier => {}
+            _ => panic!("parse s = {} failed", s),
+        }
+        match tokens.get_child_idx(4).unwrap()._type {
+            TokenType::GT => {}
+            _ => panic!("parse s = {} failed", s),
+        }
+        match tokens.get_child_idx(5).unwrap()._type {
+            TokenType::IntLiteral => {}
+            _ => panic!("parse s = {} failed", s),
+        }
+        match tokens.get_child_idx(6).unwrap()._type {
+            TokenType::SemiColon => {}
+            _ => panic!("parse s = {} failed", s),
+        }
+        match tokens.get_child_idx(7).unwrap()._type {
+            TokenType::LeftBrace => {}
+            _ => panic!("parse s = {} failed", s),
+        }
+        match tokens.get_child_idx(8).unwrap()._type {
+            TokenType::RightBrace => {}
+            _ => panic!("parse s = {} failed", s),
         }
     }
 
-    #[test]
-    fn semi_colon() {
-        let str1 = String::from(";");
-        let tokens = lexing(str1);
-        assert!(tokens.data.len() == 1);
-        match tokens.peek() {
-            Some(t) => match t._type {
-                TokenType::SemiColon => {
-                    println!("");
-                }
-                _ => panic!("token is not a SemiColon"),
-            },
-            None => panic!("parse test fail"),
-        }
-    }
-
-    #[test]
-    fn parse_test_13() {
-        let str1 = String::from(" ");
-        let tokens = lexing(str1);
-        assert!(tokens.data.len() == 0);
-        match tokens.peek() {
-            Option::None => {}
-            _ => {
-                panic!("parse_test_12 failure!")
-            }
-        }
-    }
-
-    #[test]
-    fn tokens_read() {
-        let str1 = String::from("a");
-        let mut tokens = lexing(str1.clone());
-        assert!(tokens.count() == 1);
-        let ot = tokens.read();
-        match ot {
-            Some(t) => match t._type {
-                TokenType::Identifier => {}
-                _ => panic!("token type mismatch, type is {:?}", t),
-            },
-            None => panic!("token missing"),
-        }
-    }
     #[test]
     fn tokens_set_position() {
         let str1 = String::from("int num=1; ");
