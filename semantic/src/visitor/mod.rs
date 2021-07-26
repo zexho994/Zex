06b7655d127503b2137a16eca6864bda5c8f8138
zexho <zouzhihao994@gmail.com>
2021-07-26 23:29:59 +0800

    [refactor] rename

--- semantic/src/visitor/mod.rs
+++ semantic/src/visitor/mod.rs
@@ -2,6 +2,6 @@ pub mod visit_block_statement;
 pub mod visit_program;
 pub mod visit_statement;
 pub mod visit_statements;
-pub mod visit_var_declare;
+pub mod visit_declare_variable;
 pub mod visit_declare_fn;
 pub mod visitor;
